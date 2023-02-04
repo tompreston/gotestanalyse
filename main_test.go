816131f08ca277822147e97bbcb3969f460efa16
Tom Preston <tompreston@monzo.com>
2023-02-04 11:43:56 +0000

    Add skip test

--- main_test.go
+++ main_test.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
 	"testing"
 	"time"
@@ -11,6 +10,11 @@ func TestAlwaysPass(t *testing.T) {
 	t.Log("TestAlwaysPass: This test always passes")
 }
 
+func TestAlwaysSkip(t *testing.T) {
+	t.Skip()
+	t.Error("TestAlwaysSkip: This test always skips")
+}
+
 func TestAlwaysFail(t *testing.T) {
 	t.Skip()
 	t.Error("TestAlwaysFail: This test always fails")
@@ -18,9 +22,7 @@ func TestAlwaysFail(t *testing.T) {
 
 func TestRandomFail(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
-	i := rand.Intn(100)
-	fmt.Println(i)
-	if i > 50 {
+	if rand.Intn(100) > 50 {
 		t.Log("TestRandomFail: This test passed")
 	} else {
 		t.Error("TestRandomFail: This test failed")
